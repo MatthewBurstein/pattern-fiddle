bbc341818b468e8e525a2dec7775847c0659cf98
Matt Burstein <j.matthew.burstein@gmail.com>
2019-12-01 01:58:23 +0000

    add circle rotation

---
+++ scripts/circles.js
@@ -0,0 +1,17 @@
+const CIRCLE_COUNT = 10
+const ANGLE = 360 / CIRCLE_COUNT
+
+const circlesContainers = Array.from(Array(CIRCLE_COUNT)).map((_, idx) =>
+  document.querySelector(`.circles-container-${idx}`)
+)
+
+let currentShiftIndex = 0
+function shift() {
+  currentShiftIndex = currentShiftIndex === ANGLE ? 0 : currentShiftIndex
+  circlesContainers.forEach((circles, idx) => {
+    circles.style.transform = `rotate(${idx * ANGLE + currentShiftIndex}deg)`
+  })
+  currentShiftIndex = currentShiftIndex + 0.2
+}
+
+setInterval(shift, 50)
