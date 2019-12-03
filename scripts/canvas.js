76fba008383278720dc34ec7467811069ac87691
Matt Burstein <j.matthew.burstein@gmail.com>
2019-12-03 22:40:16 +0000

    add squares algorithm to canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,61 +1,72 @@
-const canvas = document.getElementById("canvas")
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-const ctx = canvas.getContext("2d")
-const dimension = Math.floor(
-  (canvas.width > canvas.height ? canvas.width : canvas.height) / 12
-)
-
-console.log(dimension)
-
 const colors = ["#EAE0CC", "#C9ADA1", "#ADA7C9", "#FFF3BB"]
-let isInitDrawTwoColumns = false
 
-function draw(xOffset) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  let yIndex = 0
-  let xIndex = 0
+const canvas = document.getElementById("canvas")
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+const columnsToDisplay = 12
+const overflow = 2
+const dimension = canvas.width / 12
 
-  // draw first row or two rows
-  while (yIndex * dimension < canvas.height) {
-    square(0, yIndex * dimension, xOffset - dimension)
-    square(xOffset - dimension, yIndex * dimension)
-    yIndex++
-  }
+const ctx = canvas.getContext("2d")
 
-  yIndex = 0
-  while (xIndex * dimension + xOffset < canvas.width) {
-    while (yIndex * dimension < canvas.height) {
-      square(xIndex * dimension + xOffset, yIndex * dimension)
-      yIndex++
+const numberOfColumns = columnsToDisplay + 2 * overflow
+
+const numberOfRows = Math.ceil(canvas.height / dimension + 2) * overflow
+
+const colArr = Array(numberOfColumns).fill()
+const rowArr = Array(numberOfRows).fill()
+
+const squares = colArr.map((_, xIdx) => {
+  return rowArr.map((_, yIdx) => {
+    return Object.freeze({
+      xCoord: xIdx - 2,
+      yCoord: yIdx - 2,
+      isEvenColumn: xIdx % 2 === 0
+    })
+  })
+})
+console.log(squares)
+function draw(offset) {
+  for (row of squares) {
+    for (square of row) {
+      const xStart = square.xCoord * dimension + offset
+      const yStart = square.yCoord * dimension
+
+      ctx.fillStyle = getColor(square)
+      ctx.fillRect(
+        xStart < 0 ? 0 : xStart,
+        yStart < 0 ? 0 : yStart,
+        getSquareWidth(square, offset),
+        dimension
+      )
     }
-    yIndex = 0
-    xIndex++
   }
 }
 
-function square(xCoord, yCoord, width = dimension, height = dimension) {
-  ctx.fillStyle = getColor(xCoord, yCoord)
-  return ctx.fillRect(xCoord, yCoord, width, height)
-}
-
-function getColor(xIndex, yIndex) {
-  xColorCoord = Math.round(xIndex / dimension)
-  yColorCoord = Math.round(yIndex / dimension)
+function getColor(square) {
+  const { xCoord, yCoord, isEvenColumn } = square
   let colorIndex
-
-  if (xColorCoord % 2 === 0) {
-    colorIndex = yColorCoord % 2 === 0 ? 0 : 1
+  if (xCoord % 2 === 0) {
+    colorIndex = yCoord % 2 === 0 ? 0 : 1
   } else {
-    colorIndex = yColorCoord % 2 === 0 ? 2 : 3
+    colorIndex = yCoord % 2 === 0 ? 2 : 3
   }
   return colors[colorIndex]
 }
 
-let offset = 0
+function getSquareWidth(square, offset) {
+  const xStart = square.xCoord * dimension + offset
+  if (xStart + dimension < 0) {
+    return 0
+  } else if (xStart < 0) {
+    return offset
+  } else {
+    return dimension
+  }
+}
 
+let offset = 0
 setInterval(() => {
   draw(offset)
   offset = (offset + 1) % (dimension * 2)
 }, 30)
-// draw(120)
