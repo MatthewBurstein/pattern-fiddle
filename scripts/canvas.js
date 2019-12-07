4e4ee81ea72dfa359a38948793cdc14e48a13002
Matt Burstein <j.matthew.burstein@gmail.com>
2019-12-07 22:39:11 +0000

    add variable speed to squares

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,73 +1,101 @@
-const colors = ["#EAE0CC", "#C9ADA1", "#ADA7C9", "#FFF3BB"]
+;(function(module) {
+  const colors = ["#EAE0CC", "#C9ADA1", "#ADA7C9", "#FFF3BB"]
 
-const canvas = document.getElementById("canvas")
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
-const columnsToDisplay = 12
-const overflow = 2
-const dimension = canvas.width / 12
+  const canvas = document.getElementById("canvas")
+  canvas.height = window.innerHeight
+  canvas.width = window.innerWidth
+  const columnsToDisplay = 12
+  const overflow = 2
+  const dimension = canvas.width / 12
 
-const ctx = canvas.getContext("2d")
+  const ctx = canvas.getContext("2d")
 
-const numberOfColumns = columnsToDisplay + 2 * overflow
+  const numberOfColumns = columnsToDisplay + 2 * overflow
 
-const numberOfRows = Math.ceil(canvas.height / dimension + 2) * overflow
+  const numberOfRows = Math.ceil(canvas.height / dimension + 2) * overflow
 
-const colArr = Array(numberOfColumns).fill()
-const rowArr = Array(numberOfRows).fill()
+  const colArr = Array(numberOfColumns).fill()
+  const rowArr = Array(numberOfRows).fill()
 
-// make grid representation
-const squares = colArr.map((_, xIdx) => {
-  return rowArr.map((_, yIdx) => {
-    return Object.freeze({
-      xCoord: xIdx - 2,
-      yCoord: yIdx - 2,
-      isEvenColumn: xIdx % 2 === 0
+  // make grid representation
+  const squares = colArr.map((_, xIdx) => {
+    return rowArr.map((_, yIdx) => {
+      return Object.freeze({
+        xCoord: xIdx - 2,
+        yCoord: yIdx - 2
+      })
     })
   })
-})
 
-function draw(offset) {
-  for (row of squares) {
-    for (square of row) {
-      const xStart = square.xCoord * dimension + offset
-      const yStart = square.yCoord * dimension
+  function draw(offset) {
+    for (row of squares) {
+      for (square of row) {
+        const xStart = square.xCoord * dimension + offset
+        const yStart = square.yCoord * dimension
 
-      ctx.fillStyle = getColor(square)
-      ctx.fillRect(
-        xStart < 0 ? 0 : xStart,
-        yStart < 0 ? 0 : yStart,
-        getSquareWidth(square, offset),
-        dimension
-      )
+        ctx.fillStyle = getColor(square)
+        ctx.fillRect(
+          xStart < 0 ? 0 : xStart,
+          yStart < 0 ? 0 : yStart,
+          getSquareWidth(square, offset),
+          dimension
+        )
+      }
     }
   }
-}
 
-function getColor(square) {
-  const { xCoord, yCoord, isEvenColumn } = square
-  let colorIndex
-  if (xCoord % 2 === 0) {
-    colorIndex = yCoord % 2 === 0 ? 0 : 1
-  } else {
-    colorIndex = yCoord % 2 === 0 ? 2 : 3
+  function getColor(square) {
+    const { xCoord, yCoord } = square
+    let colorIndex
+    if (xCoord % 2 === 0) {
+      colorIndex = yCoord % 2 === 0 ? 0 : 1
+    } else {
+      colorIndex = yCoord % 2 === 0 ? 2 : 3
+    }
+    return colors[colorIndex]
   }
-  return colors[colorIndex]
-}
 
-function getSquareWidth(square, offset) {
-  const xStart = square.xCoord * dimension + offset
-  if (xStart + dimension < 0) {
-    return 0
-  } else if (xStart < 0) {
-    return offset
-  } else {
-    return dimension
+  function getSquareWidth(square, offset) {
+    const xStart = square.xCoord * dimension + offset
+    if (xStart + dimension < 0) {
+      return 0
+    } else if (xStart < 0) {
+      return offset
+    } else {
+      return dimension
+    }
   }
-}
 
-let offset = 0
-setInterval(() => {
-  draw(offset)
-  offset = (offset + 1) % (dimension * 2)
-}, 30)
+  let offset = 0
+  function itterate() {
+    draw(offset)
+    offset = (offset + 1) % (dimension * 2)
+  }
+
+  function startSquareAnimation(timePerSquare) {
+    const startTimestamp = Date.now()
+    const baseStepTime = timePerSquare / dimension
+
+    function recursivelyAnimateSquares(time) {
+      setTimeout(() => {
+        window.requestAnimationFrame(itterate)
+        recursivelyAnimateSquares(getInterval())
+      }, time)
+    }
+
+    function getInterval() {
+      const currentTimePosition = (Date.now() - startTimestamp) % interval
+      const rateOfAccelerationCalibrator = 1.2
+      return currentTimePosition < interval / 2
+        ? baseStepTime -
+            Math.log(currentTimePosition) * rateOfAccelerationCalibrator
+        : baseStepTime +
+            Math.log(currentTimePosition) * rateOfAccelerationCalibrator
+    }
+
+    recursivelyAnimateSquares(baseStepTime)
+  }
+
+  const interval = 1400
+  startSquareAnimation(interval)
+})(this)
--- scripts/circles.js
+++ scripts/circles.js
@@ -1,17 +1,19 @@
-const CIRCLE_COUNT = 10
-const ANGLE = 360 / CIRCLE_COUNT
+;(function() {
+  const CIRCLE_COUNT = 10
+  const ANGLE = 360 / CIRCLE_COUNT
 
-const circlesContainers = Array.from(Array(CIRCLE_COUNT)).map((_, idx) =>
-  document.querySelector(`.circles-container-${idx}`)
-)
+  const circlesContainers = Array.from(Array(CIRCLE_COUNT)).map((_, idx) =>
+    document.querySelector(`.circles-container-${idx}`)
+  )
 
-let currentShiftIndex = 0
-function shift() {
-  currentShiftIndex = currentShiftIndex === ANGLE ? 0 : currentShiftIndex
-  circlesContainers.forEach((circles, idx) => {
-    circles.style.transform = `rotate(${idx * ANGLE + currentShiftIndex}deg)`
-  })
-  currentShiftIndex = currentShiftIndex + 0.2
-}
+  let currentShiftIndex = 0
+  function shift() {
+    currentShiftIndex = currentShiftIndex === ANGLE ? 0 : currentShiftIndex
+    circlesContainers.forEach((circles, idx) => {
+      circles.style.transform = `rotate(${idx * ANGLE + currentShiftIndex}deg)`
+    })
+    currentShiftIndex = currentShiftIndex + 0.2
+  }
 
-setInterval(shift, 50)
+  setInterval(shift, 50)
+})(this)
