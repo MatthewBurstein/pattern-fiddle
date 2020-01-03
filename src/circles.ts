5d6951534ed6d270ef158520dfef9a924bfa6918
Matt Burstein <j.matthew.burstein@gmail.com>
2020-01-03 16:34:04 +0000

    refactor circles following addition of typescript

--- src/circles.ts
+++ src/circles.ts
@@ -1,19 +1,17 @@
-// ;(function() {
-//   const CIRCLE_COUNT = 10
-//   const ANGLE = 360 / CIRCLE_COUNT
+  const CIRCLE_COUNT = 10
+  const ANGLE = 360 / CIRCLE_COUNT
 
-//   const circlesContainers = Array.from(Array(CIRCLE_COUNT)).map((_, idx) =>
-//     document.querySelector(`.circles-container-${idx}`)
-//   )
+  const circlesContainers: HTMLElement[] = Array.from(Array(CIRCLE_COUNT)).map((_, idx) =>
+    document.querySelector(`.circles-container-${idx}`)
+  )
 
-//   let currentShiftIndex = 0
-//   function shift() {
-//     currentShiftIndex = currentShiftIndex === ANGLE ? 0 : currentShiftIndex
-//     circlesContainers.forEach((circles, idx) => {
-//       circles.style.transform = `rotate(${idx * ANGLE + currentShiftIndex}deg)`
-//     })
-//     currentShiftIndex = currentShiftIndex + 0.2
-//   }
+  let currentShiftIndex = 0
+  function shift(): void {
+    currentShiftIndex = currentShiftIndex === ANGLE ? 0 : currentShiftIndex
+    circlesContainers.forEach((circles, idx) => {
+      circles.style.transform = `rotate(${idx * ANGLE + currentShiftIndex}deg)`
+    })
+    currentShiftIndex = currentShiftIndex + 0.2
+  }
 
-//   setInterval(shift, 50)
-// })(this)
+  setInterval(shift, 50)
