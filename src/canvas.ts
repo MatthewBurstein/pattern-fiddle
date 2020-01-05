e8d7a798ca3a53a299cd12b4e3400dcbeea4a852
Matt Burstein <j.matthew.burstein@gmail.com>
2020-01-05 20:15:49 +0000

    add touch events

--- src/canvas.ts
+++ src/canvas.ts
@@ -150,3 +150,12 @@ window.addEventListener('mousemove', event => {
   const { clientX, clientY } = event;
   handleMouseMove(clientX, clientY);
 });
+
+window.addEventListener('touchstart', event => {
+  const { pageX, pageY } = event.touches[0]
+  handleMouseMove(pageX, pageY);
+});
+window.addEventListener('touchmove', event => {
+  const { pageX, pageY } = event.touches[0]
+  handleMouseMove(pageX, pageY);
+});
